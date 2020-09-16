6bcbc934775386744e9f638789a7184a0827c736
zd <zd577125271@outlook.com>
2020-09-16 22:16:28 +0800

    第二次修改图片

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <h2>演示</h2>
     <img src="http://file.qqtouxiang.com/qinglv/2020-07-21/1b00fe541d4533ecd0df3fa39d02ab2f.jpeg">
+    <img src="https://img1.gamersky.com/upimg/pic/2020/09/16/202009161026064213_small.jpg">
     </div>
 </template>
 
