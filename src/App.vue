48b1a03fe42e5017e09464e4cc4cb002913470ad
zd <zd577125271@outlook.com>
2020-09-16 22:24:47 +0800

    第三次修改图片

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,9 @@
     <h2>演示</h2>
     <img src="http://file.qqtouxiang.com/qinglv/2020-07-21/1b00fe541d4533ecd0df3fa39d02ab2f.jpeg">
     <img src="https://img1.gamersky.com/upimg/pic/2020/09/16/202009161026064213_small.jpg">
+    <img src="https://img1.gamersky.com/upimg/pic/2020/09/10/202009101215319136_small.jpg">
+
+
     </div>
 </template>
 
