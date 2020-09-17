f77c53e5f878335bbcf2478f48d307455d982594
zd <zd577125271@outlook.com>
2020-09-17 10:35:27 +0800

    第八次修改图片

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,8 @@
 
     <img src="https://img1.gamersky.com/upimg/pic/2019/01/02/201901021142595201_small.jpg">
     <img src="https://images.ali213.net/picfile/pic/2020/07/14/927_2020071414446759.jpg">
+    <img src="https://img1.gamersky.com/upimg/pic/2020/08/31/202008311429181003_small.jpg">
+
     </div>
 </template>
 
